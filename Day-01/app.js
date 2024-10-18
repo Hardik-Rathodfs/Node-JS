b5510b7a53954908e405a45e0f15b3669870c3f3
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2024-10-18 12:21:49 +0530

    Day-1 of node

---
+++ Day-01/app.js
@@ -0,0 +1,10 @@
+
+const add=(a,b)=>{
+    return a+b;
+}
+
+console.log('====================================');
+console.log(4,5);
+console.log('====================================');
+
+module.exports=add

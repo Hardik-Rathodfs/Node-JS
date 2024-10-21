acee4785fbb1ff8a87adc68a40449bedd2b77077
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2024-10-21 12:42:56 +0530

    Project-1 OF Node js

---
+++ Day-04 (Project)/app.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+// const { log } = require("console");
+
+let server = http.createServer((req,res)=>{
+    if(req.url=="/")
+    {
+        let a =fs.readFileSync("index.html","utf-8")
+        res.end(a);
+    }
+    else if(req.url=="/about")
+    {
+        let b =fs.readFileSync("about.html","utf-8")
+        res.end(b)
+    }
+    else if(req.url=="/contact")
+    {
+        let c=fs.readFileSync("contact.html","utf-8")
+        res.end(c)
+    }
+    else{
+        res.end("page not found..!!")
+    }
+})
+
+server.listen(3000,()=>{
+    console.log("Server Running on 3000");
+    
+})

7bb648f2af642c49ef84f4ed45fb8c56bc2fd43e
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2024-10-23 13:08:54 +0530

    Day-05_Roting

--- Day-05/app.js
+++ Day-05/app.js
@@ -1,1 +1,13 @@
-console.log("hello");
+
+const express = require("express")
+
+let app = express()
+
+app.get("/",(req,res)=>{
+res.render("index.html")
+})
+
+app.listen(9000,()=>{
+    console.log("Server is running on 9000");
+    
+})

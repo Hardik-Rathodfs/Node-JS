3bb2278fdaac9f7d3e24c55b281a16ce2012d2dc
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2024-12-13 12:07:54 +0530

    Signup_Login_With_EJS

--- Day-19/Routes/UserRouter.js
+++ Day-19/Routes/UserRouter.js
@@ -1,56 +1,63 @@
-const {Router}= require("express")
-const passport=require("passport")
+const {Router} = require("express")
+const passport = require("passport")
 const UserModel = require("../Model/UserModel")
 const LocalStrategey = require("passport-local").Strategy
 
-const UserRouter=Router()
+const UserRouter = Router()
 
 passport.use(new LocalStrategey(
-    async(username,password,done)=>{
-        let user=await UserModel.findOne({username})
-        if(!user)
-        {
-            return done(null,false,{msg:"User not registerd!!"})
+    async(username, password, done) => {
+        let user = await UserModel.findOne({username})
+        if(!user) {
+            return done(null, false, {msg: "User not registered!!"})
         }
-        if(user.password != password)
-        {
-            return done(null,false,{msg:"Incorrect password"})
+        if(user.password != password) {
+            return done(null, false, {msg: "Incorrect password"})
         }
-        return done(null,user)
+        return done(null, user)
     }
 ))
 
-passport.serializeUser((user,done)=>{
-    done(null,user.id)
+passport.serializeUser((user, done) => {
+    done(null, user.id)
 })
 
-passport.deserializeUser(async(id,done)=>{
+passport.deserializeUser(async(id, done) => {
     let user = await UserModel.findById(id)
-    done(null,user)
+    done(null, user)
 })
 
-UserRouter.get("/",(req,res)=>{
+UserRouter.get("/", (req, res) => {
     res.render("signup.ejs")
 })
 
-UserRouter.post("/",async(req,res)=>{
-    try{
-        let data=req.body
-        let ans=await UserModel.create(data)
-        res.status(200).send({data:ans,msg:"Register Successfully"})
+UserRouter.post("/", async(req, res) => {
+    try {
+        let data = req.body
+        let ans = await UserModel.create(data)
+        res.status(200).redirect('/login')
     }
-    catch(error)
-    {
-        res.status(501).send({err:error.message})
+    catch(error) {
+        res.status(501).send({err: error.message})
     }
 })
 
-UserRouter.get("/login",(req,res)=>{
+UserRouter.get("/login", (req, res) => {
     res.render("login.ejs")
 })
 
-UserRouter.post("/login",passport.authenticate("local"),async(req,res)=>{
-    res.send({msg:"User Login Successfully"})
+UserRouter.get("/home", (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render("home.ejs")
+    } else {
+        res.redirect("/login")
+    }
 })
 
-module.exports=UserRouter
+UserRouter.post("/login", passport.authenticate("local", {
+    successRedirect: "/home", 
+    failureRedirect: "/login", 
+    failureFlash: true          
+}))
+
+module.exports = UserRouter

9e0a46fedb4485a211579b8b650634e0b70c5eb6
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2025-02-13 18:24:30 +0530

    E-commerce

--- COZA Store/frontend/src/Routes/MainRoutes.jsx
+++ COZA Store/frontend/src/Routes/MainRoutes.jsx
@@ -4,6 +4,7 @@ import Login from '../Pages/Login'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from '../Pages/Home'
 import ForgotPass from '../Pages/ForgotPass'
+import Admin from '../Pages/Admin'
 
 const MainRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const MainRoutes = () => {
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/forgotpassword" element={<ForgotPass/>}/>
+        <Route path="/admin" element={<Admin/>}></Route>
         </Routes>
     </div>
   )
